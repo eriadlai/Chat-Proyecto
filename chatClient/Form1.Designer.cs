28f198b48d0d269659982896a01e051948e9a2c7
Adlai CG <42752044+eriadlai@users.noreply.github.com>
2023-02-01 12:57:43 -0800

    Update

---
+++ chatClient/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace chatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbUsuario = new System.Windows.Forms.TextBox();
+            this.tbMensaje = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbLogMensajes = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbDir = new System.Windows.Forms.TextBox();
+            this.tbDestiny = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbUsuario
+            // 
+            this.tbUsuario.Location = new System.Drawing.Point(74, 22);
+            this.tbUsuario.Name = "tbUsuario";
+            this.tbUsuario.Size = new System.Drawing.Size(307, 23);
+            this.tbUsuario.TabIndex = 0;
+            // 
+            // tbMensaje
+            // 
+            this.tbMensaje.Location = new System.Drawing.Point(20, 476);
+            this.tbMensaje.Name = "tbMensaje";
+            this.tbMensaje.Size = new System.Drawing.Size(387, 23);
+            this.tbMensaje.TabIndex = 2;
+            this.tbMensaje.TextChanged += new System.EventHandler(this.tbMensaje_TextChanged);
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(74, 80);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(307, 23);
+            this.btnConectar.TabIndex = 3;
+            this.btnConectar.Text = "Conectarse";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(20, 505);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(387, 23);
+            this.btnEnviar.TabIndex = 4;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Usuario:";
+            // 
+            // rtbLogMensajes
+            // 
+            this.rtbLogMensajes.Location = new System.Drawing.Point(20, 124);
+            this.rtbLogMensajes.Name = "rtbLogMensajes";
+            this.rtbLogMensajes.Size = new System.Drawing.Size(387, 302);
+            this.rtbLogMensajes.TabIndex = 6;
+            this.rtbLogMensajes.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbDir);
+            this.groupBox1.Controls.Add(this.tbUsuario);
+            this.groupBox1.Controls.Add(this.btnConectar);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(20, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(387, 113);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Conexión";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "IP:";
+            // 
+            // tbDir
+            // 
+            this.tbDir.Location = new System.Drawing.Point(74, 51);
+            this.tbDir.Name = "tbDir";
+            this.tbDir.Size = new System.Drawing.Size(307, 23);
+            this.tbDir.TabIndex = 6;
+            // 
+            // tbDestiny
+            // 
+            this.tbDestiny.Enabled = false;
+            this.tbDestiny.Location = new System.Drawing.Point(76, 443);
+            this.tbDestiny.Name = "tbDestiny";
+            this.tbDestiny.Size = new System.Drawing.Size(331, 23);
+            this.tbDestiny.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 446);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Destino:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 542);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbDestiny);
+            this.Controls.Add(this.rtbLogMensajes);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.tbMensaje);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox tbUsuario;
+        private TextBox tbMensaje;
+        private Button btnConectar;
+        private Button btnEnviar;
+        private Label label1;
+        private RichTextBox rtbLogMensajes;
+        private GroupBox groupBox1;
+        private Label label2;
+        private TextBox tbDir;
+        private Label label3;
+        private TextBox tbDestiny;
+    }
+}
